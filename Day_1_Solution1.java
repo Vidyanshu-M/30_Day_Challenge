be01ee34e2fa58c28d6a419e4abffa434f4c9906
vidyanshumishra-byte <vidyanshumishra99@gmail.com>
2026-07-16 21:50:27 +0530

    Solution 2 of day 1

--- Day_1_Solution1.java
+++ Day_1_Solution1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Day_1_Solution {
+public class Day_1_Solution1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
---
+++ Day_1_Solution2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Day_1_Solution2 { 
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in); 
+        
+        String inputString = scan.nextLine(); 
+        
+        System.out.println("Hello, World.");
+        
+        System.out.println(inputString);
+        scan.close(); 
+    }
+}
+
