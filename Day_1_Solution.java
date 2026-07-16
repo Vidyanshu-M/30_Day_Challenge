4d2b4eda1f26a3da7e3f62d24f764b8e4dde064f
vidyanshumishra-byte <vidyanshumishra99@gmail.com>
2026-07-16 21:02:07 +0530

    Initial commit

---
+++ Day_1_Solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Day_1_Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
+        int i2;
+        double d2;
+        String s2;
+
+
+
+        System.out.print("Enter an integer: ");
+        i2 = scan.nextInt();
+        System.out.print("Enter a double: ");
+        d2 = scan.nextDouble();
+        scan.nextLine();
+        System.out.print("Enter a string: ");
+        s2 = scan.nextLine();
+
+        System.out.println(i + i2);
+        System.out.println(d + d2);
+        System.out.println(s + s2);
+
+        scan.close();
+    }
+}
